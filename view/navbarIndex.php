c6ffd4151ccf88331132a1d42c6af5441b956dea
Malene <maleneklit@hotmail.com>
2017-08-03 13:16:36 +0200

    navbarIndex
    navbarIndex - solves bug

--- view/navbarIndex.php
+++ view/navbarIndex.php
@@ -1,18 +1,21 @@
 <!-- navbar indexseite  -->
     <nav class="navbar navbar-custom navbar-fixed-top index" id="navBar">
-            <div class="navbar-header img-responsive">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" id="menuToggle">
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                </button>
+        <div class="navbar-header img-responsive">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+                <a class="navbar-brand" href="./index.php"><img src="img/imgsolga/logoCat1.jpg" alt="logo" style="height: 40px"></a>
             </div>
             <div class="collapse navbar-collapse" id="myNavbar">
                 <ul class="nav navbar-nav navbar-right" id="menu">
-                    <li class="active"><a href="./about.php">A B O U T</a></li>
+                    <li><a href="./about.php">A B O U T</a></li>
                     <li><a href="./collection.php">C O LL E C T I O N</a></li>
-                    <li><a href="./press.php">P R E SS</a></li>
+                    <li>
+                        <a href="./press.php">P R E SS</a>
+                    </li>
                 </ul>
             </div>
     </nav>
-    <div id="pushBack"></div>
+<div id="pushBack"></div>
