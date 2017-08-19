272a53339f5aa571317d988c5eab92e25a5e4568
mallle <maleneklit@hotmail.com>
2017-08-19 22:11:08 +0200

    Backgroundcolor on Active navbar

--- press.php
+++ press.php
@@ -1,9 +1,25 @@
 <?php
  include_once 'view/header.php';
 
- include_once 'view/navbar.php';
-
 ?>
+    <nav class="navbar navbar-custom navbar-fixed-top navbarAll">
+      <div class="navbar-header img-responsive">  
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>  
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+          <a class="navbar-brand" href="./index.php"><img src="img/imgsolga/logoCat1.jpg" alt="logo" style="height: 60px"></a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav navbar-right">   
+          <li><a href="./about.php">A B O U T</a></li>
+          <li><a href="./collection.php">C O LL E C T I O N</a></li>
+          <li class="active"><a href="./press.php">P R E SS</a></li>
+        </ul>  
+      </div>
+    </nav>
+
 <div class="wrapper">
 
 
